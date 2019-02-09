bfb61a5804b0d4f2b383fc93316536fa46886095
Tomek Stacharski <twtstacharski@gmail.com>
2019-02-09 08:27:55 +0800

    completed CRUDs. Addding styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles, only: [:index, :show, :new, :create, :destroy, :update, :edit]
+  # get 'articles/index'
+  # get 'articles/show'
+  # get 'articles/new'
+  # get 'articles/create'
+  # get 'articles/destroy'
+  # get 'articles/edit'
+  # get 'articles/update'
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 10.times{
+#   Article.create(
+#     {title: Faker::Food.dish,
+#       content: Faker::Food.description
+#   })
+# }
+# 9.times{
+#   Article.create([
+#     { title: Faker::Food.dish },
+#     { content: Faker::Food.description }
+#     ])
+# }
